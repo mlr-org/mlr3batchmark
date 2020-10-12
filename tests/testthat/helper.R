7f026af8d0fe0d89ef805a4bb74ce03e1b39ee2b
Patrick Schratz <patrick.schratz@gmail.com>
2020-10-12 08:32:12 +0200

    Load mlr3 and checkmate in tests (#1)

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,2 +1,7 @@
-lapply(list.files(system.file("testthat", package = "mlr3"), pattern = "^helper.*\\.[rR]",
-                  full.names = TRUE), source)
+library(mlr3)
+library(checkmate)
+library(testthat)
+
+lapply(list.files(system.file("testthat", package = "mlr3"),
+  pattern = "^helper.*\\.[rR]",
+  full.names = TRUE), source)

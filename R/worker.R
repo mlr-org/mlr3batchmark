96b49180f44ac7c2effe7d4f4524ea8532b8589d
be-marc <marcbecker@posteo.de>
2024-11-14 16:50:46 +0100

    feat: sleep to measure memory

--- R/worker.R
+++ R/worker.R
@@ -2,6 +2,9 @@ run_learner = function(job, data, learner_hash, param_values_hash, store_models,
   if (!is.null(renv_project)) {
     renv::load(renv_project)
   }
+
+  Sys.sleep(30)
+
   workhorse = utils::getFromNamespace("workhorse", ns = asNamespace("mlr3"))
   resampling = get(job$prob.pars$resampling_hash, envir = .GlobalEnv)
   learner = get(learner_hash, envir = .GlobalEnv)
@@ -9,7 +12,7 @@ run_learner = function(job, data, learner_hash, param_values_hash, store_models,
 
   if (!is.null(param_values)) learner$param_set$set_values(.values = param_values)
 
-  workhorse(
+  result = workhorse(
     iteration = job$repl,
     task = data,
     learner = learner,
@@ -18,4 +21,8 @@ run_learner = function(job, data, learner_hash, param_values_hash, store_models,
     lgr_threshold = lgr::get_logger("mlr3")$threshold,
     is_sequential = FALSE
   )
+
+  Sys.sleep(30)
+
+  return(result)
 }

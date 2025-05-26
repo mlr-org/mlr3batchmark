a1a2be83820cad09a79e12718fe70c9389d69feb
Marc Becker <33069354+be-marc@users.noreply.github.com>
2025-05-26 09:17:11 +0200

    compatibility: mlr3 1.0.0 (#36)

--- R/worker.R
+++ R/worker.R
@@ -9,13 +9,26 @@ run_learner = function(job, data, learner_hash, param_values_hash, store_models,
 
   if (!is.null(param_values)) learner$param_set$set_values(.values = param_values)
 
-  workhorse(
-    iteration = job$repl,
-    task = data,
-    learner = learner,
-    resampling = resampling,
-    store_models = store_models,
-    lgr_threshold = lgr::get_logger("mlr3")$threshold,
-    is_sequential = FALSE
-  )
+  if (packageVersion("mlr3") > "0.23.0") {
+    workhorse(
+      iteration = job$repl,
+      task = data,
+      learner = learner,
+      resampling = resampling,
+      store_models = store_models,
+      lgr_index = lgr::logger_index(),
+      is_sequential = FALSE
+    )
+  } else {
+    workhorse(
+      iteration = job$repl,
+      task = data,
+      learner = learner,
+      resampling = resampling,
+      store_models = store_models,
+      lgr_threshold = lgr::get_logger("mlr3")$threshold,
+      is_sequential = FALSE
+    )
+  }
 }
+
